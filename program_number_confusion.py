982fd1c71ff6eaa1a8d3d9701253595ef77e59c3
Aidshiv <163630330+Aidshiv@users.noreply.github.com>
2025-06-05 23:31:47 +0530

    program_number_confusion.py

---
+++ program_number_confusion.py
@@ -0,0 +1,19 @@
+
+  | In Test | In Lab Manual |
+| ------- | ------------- |
+| 1       | 2             |
+| 2       | 3             |
+| 3       | 4             |
+| 4       | 6             |
+| 5       | 7             |
+| 6       | 8             |
+
+
+| In Test | In Lab Manual                      |
+| ------- | ---------------------------------- |
+| 1       | Experiment 2 - Working with Maven  |
+| 2       | Experiment 3 - Working with Gradle |
+| 3       | Experiment 4 - Migrate to Gradle   |
+| 4       | Experiment 6 - Jenkins and GitHub  |
+| 5       | Experiment 7 - Using Ansible       |
+| 6       | Experiment 8 - Jenkins and Ansible |
